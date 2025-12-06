c256b4c46d744ac5ea8e17077fcc007427af4ec9
ViniiR <cvrm2004@gmail.com>
2025-12-05 23:04:03 -0300

    fix: remove error_handler

--- bin/main.ml
+++ bin/main.ml
@@ -1,31 +1,5 @@
 open Lib
 
-(* let error_handler (err : Dream.error) _debug_info (response : Dream.response) = *)
-(*   let module DbErr = Database.Types.Errors in *)
-(*   let module BookErr = Book.Types.Errors in *)
-(*   let log (msg : string) = *)
-(*     Printf.eprintf "Error: %s\n" msg; *)
-(*     flush stderr *)
-(*   in *)
-(*   let message, (status : Dream.status) = *)
-(*     match err.condition with *)
-(*     | `Exn exn -> ( *)
-(*         match exn with *)
-(*         (* Database *) *)
-(*         | DbErr.Failed_Database_Connection msg -> (msg, `Internal_Server_Error) *)
-(*         | DbErr.Missing_Env_Variable msg -> (msg, `Internal_Server_Error) *)
-(*         | DbErr.Failed_To_Fetch msg -> (msg, `Bad_Request) *)
-(*         (* Book routes *) *)
-(*         (* | BookErr.InvalidId msg -> (msg, `Bad_Request) *) *)
-(*         (* Unhandled *) *)
-(*         | _ -> ("Internal Exception", `Internal_Server_Error)) *)
-(*     | _ -> ("Internal Error", `Internal_Server_Error) *)
-(*   in *)
-(*   log message; *)
-(*   Dream.set_status response status; *)
-(*   Dream.set_body response message; *)
-(*   Lwt.return response *)
-
 let () =
   Dream.logger
   @@ Dream.router
@@ -47,5 +21,4 @@ let () =
              Root.render ~text:"We do not serve HTML ;)" |> Dream.html);
          Dream.any "/**" (fun _ -> Not_found.render |> Dream.html);
        ]
-  |> Dream.run
-       ~port:5001 (*~error_handler:(Dream.error_template error_handler)*)
+  |> Dream.run ~port:5001
